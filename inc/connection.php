297d3d45c3b7011e78d2e14bb643c9f969402da0
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2017-02-15 21:50:54 -0600

    fixed code so notice did not appear

--- inc/connection.php
+++ inc/connection.php
@@ -1,18 +1,28 @@
 <?php
-try {
   $db = new PDO("pgsql:host=localhost;port=5432;dbname=testdb;user=benjaminjohnson;");
     // set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO names(first_name, last_name)
-    VALUES ('John', 'Doe')";
-    // use exec() because no results are returned
-    $db->exec($sql);
-    echo "New record created successfully";
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
+    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (isset($_POST['first_name']))
+{
+  $sql = "INSERT INTO names(first_name, last_name)
+  VALUES ('$_POST[first_name]','$_POST[last_name]')";
+  // use exec() because no results are returned
+  $db->exec($sql);
+//     }
+// catch(PDOException $e)
+//     {
+//     echo $sql . "<br>" . $e->getMessage();
+//     }
+
+$db = null;
+  // Instructions if $_POST['value'] exist
+}
+
+//     }
+// catch(PDOException $e)
+//     {
+//     echo $sql . "<br>" . $e->getMessage();
+//     }
 
 $db = null;
 
--- index.php
+++ index.php
@@ -5,13 +5,9 @@ include("inc/connection.php")
 ?>
 <h1>Hello</h1>
 <form method="post">
-<input type="text" name="username" value=""><br>
+<input type="text" name="first_name" value=""><br>
+<input type="text" name="last_name" value=""><br>
 <input type="submit" value="send">
 </form>
-<?php
-if(!empty($_POST)){
-$username = $_POST['username'];
-echo $username;
-}
- ?>
+
 <?php include("inc/footer.php"); ?>
