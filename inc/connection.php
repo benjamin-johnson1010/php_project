389ac63f7ae25e6436492d1c252f17340a7f800b
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2017-02-16 16:02:02 -0600

    sql post and get working

--- inc/connection.php
+++ inc/connection.php
@@ -2,6 +2,15 @@
   $db = new PDO("pgsql:host=localhost;port=5432;dbname=testdb;user=benjaminjohnson;");
     // set the PDO error mode to exception
     // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+       $query = "SELECT * FROM names";
+
+       foreach ($db->query($query) as $row) {
+       print $row['first_name'] . "\t";
+       print $row['last_name'] . "\t";
+       print $row['id'];
+   }
+
     if (isset($_POST['first_name']))
 {
   $sql = "INSERT INTO names(first_name, last_name)
